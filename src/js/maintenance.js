9ee3bf1107954f05b5d1bdd1a3a8154d10ec820e
AyumuNekozuki <ayumu@nekozuki.me>
2020-11-18 06:30:52 +0900

    ニコ生メンテナンスページに対応

---
+++ src/js/maintenance.js
@@ -0,0 +1,12 @@
+var getPageTitle = document.title;
+console.log(getPageTitle);
+
+const maintenance = ["ただいまメンテナンス中です。 - ニコニコ生放送"];
+
+if(getPageTitle.includes(maintenance)){
+  chrome.storage.local.get(["setting"], function (items) {
+    if(items.setting == "true"){
+      $('body').eq(0).addClass('niconico-darkmode-maintenance');
+    }
+  });
+}
