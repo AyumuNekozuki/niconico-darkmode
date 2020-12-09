0bbba758b0eed9914da28666a1a6b4a4d71fe7a8
AyumuNekozuki <ayumu@nekozuki.me>
2020-12-09 20:53:43 +0900

    fix #55

--- src/js/maintenance.js
+++ src/js/maintenance.js
@@ -1,6 +1,9 @@
 var getPageTitle = document.title;
 
-const maintenance = ["ただいまメンテナンス中です。 - ニコニコ生放送"];
+const maintenance = [
+  "ただいまメンテナンス中です。 - ニコニコ生放送",
+  "ただいま混雑しています。 - ニコニコ生放送"
+];
 
 if(getPageTitle.includes(maintenance)){
   chrome.storage.local.get(["setting"], function (items) {
