9db5dac0979edc43084211e355fbea7bb40d0278
AyumuNekozuki <ayumu@nekozuki.me>
2021-02-26 00:31:21 +0900

    iframe系に対応

--- src/js/single-page_action.js
+++ src/js/single-page_action.js
@@ -6,16 +6,10 @@ chrome.storage.local.get(["version"], function (items) {
 
 chrome.storage.local.get(["setting"], function (items) {
   console.log("niconico-Darkmode: Setting: " + items.setting);
-  i = "yes";
   if(items.setting == "true"){
+    i = "yes";
     $('body').eq(0).addClass('niconico-darkmode-setting-true');
   }
 });
 
 console.log("niconico-Darkmode: このページでは設定の切り替えはできません。");
-
-$(document).ready( function(){
-  if(i = "yes"){
-    $('body').eq(0).addClass('niconico-darkmode-setting-true');
-  }
-});
