b389bcf9d6fd7cb5acb0881d9d2adc63d210d291
AyumuNekozuki <ayumu@nekozuki.me>
2021-03-31 05:37:33 +0900

    fix #80, fix #84, fix #85, fix #86

--- src/js/main.js
+++ src/js/main.js
@@ -34,9 +34,9 @@ chrome.storage.local.get(["version"], function (items) {
   console.log("niconico-Darkmode: version: " + items.version);
 
   //v1.2.1以降更新時、versionのsocial_topのset指定を消すこと
-  if ((items.version != "2.0.1")) {
+  if ((items.version != "2.0.3")) {
     chrome.storage.local.set({
-      "version": "2.0.1"
+      "version": "2.0.3"
     })
     chrome.storage.local.get(["setting"], function (items) {
       if (items.setting == undefined) {
