f472f83aa342debc4a55b3cdbb64323e25e14710
AyumuNekozuki <ayumu@nekozuki.me>
2021-04-07 02:37:29 +0900

    fix #89

--- src/js/single-page_action.js
+++ src/js/single-page_action.js
@@ -1,15 +1,23 @@
-var i = "no";
+var host = location.hostname + "";
+if(host == "jk.nicovideo.jp"){
+  single_page_action();
+  setTimeout(single_page_action, 200);
+}else{
+  single_page_action();
+}
 
-chrome.storage.local.get(["version"], function (items) {
-  console.log("niconico-Darkmode: version: " + items.version);
-});
+function single_page_action() {
+  chrome.storage.local.get(["version"], function (items) {
+    console.log("niconico-Darkmode: version: " + items.version);
+  });
+  
+  chrome.storage.local.get(["setting"], function (items) {
+    console.log("niconico-Darkmode: Setting: " + items.setting);
+    if(items.setting == "true"){
+      $('body').eq(0).addClass('niconico-darkmode-setting-true');
+    }
+  });
+}
 
-chrome.storage.local.get(["setting"], function (items) {
-  console.log("niconico-Darkmode: Setting: " + items.setting);
-  if(items.setting == "true"){
-    i = "yes";
-    $('body').eq(0).addClass('niconico-darkmode-setting-true');
-  }
-});
 
 console.log("niconico-Darkmode: このページでは設定の切り替えはできません。");
