0b66a361732c04f5549557206554313fdfa93e51
AyumuNekozuki <ayumu@nekozuki.me>
2021-04-07 02:20:54 +0900

    fix #91

--- src/js/iframe_action.js
+++ src/js/iframe_action.js
@@ -1,21 +1,8 @@
-var i = "no";
-
 chrome.storage.local.get(["setting"], function (items) {
   if(items.setting == "true"){
-    i = "yes";
     $('body').eq(0).addClass('niconico-darkmode-setting-true');
   }
 });
 
 console.log("niconico-Darkmode-iframe: 起動中です。");
-
-/*例外*/
-if(i="yes"){
-  var host = location.hostname;
-  var path = location.pathname;
-  var now_location = host + path + "";
-  if(now_location = "https://site.nicovideo.jp/badge/"){
-    $('body').eq(0).addClass('niconico-darkmode-setting-true');
-  }
-}
 
